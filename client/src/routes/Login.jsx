d2287b612ca5eab30b7b731aa21ec2a1f8da912b
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2024-12-14 23:33:32 +0300

    Add the formik validation

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -2,8 +2,24 @@ import React from "react";
 import "./login.css";
 import { useNavigate } from "react-router-dom";
 import animePhoto from "../assets/17f4ec064cad10da1739a11d9b293c09-removebg-preview.png";
+import { Field, Form, ErrorMessage, Formik } from "formik";
+import * as Yup from "yup";
 function Login() {
   const navigate = useNavigate();
+  const handleLogin = (values) => {
+    console.log(values);
+  };
+  const initialValues = {
+    userName: "",
+    password: "",
+  };
+  const validationSchema = Yup.object({
+    username: Yup.string().min(2, "Too short").required("Field cant be empty"),
+    password: Yup.string()
+      .min(3, "Password too short")
+      .required("Field cant be empty"),
+  });
+
   return (
     <div className="container">
       <div className="row   my-5 rounded reg ">
@@ -17,26 +33,53 @@ function Login() {
               </span>
             </p>
           </div>
-          <form
-            className="form-group d-flex flex-column gap-4  rounded auth justify-content-center "
-            action=""
+          <Formik
+            initialValues={initialValues}
+            onSubmit={handleLogin}
+            validationSchema={validationSchema}
           >
-            <input
-              type="text"
-              placeholder="Username/Email.."
-              className="form-control"
-            />
+            {(formik) => (
+              <form
+                className="form-group d-flex flex-column gap-4  rounded auth justify-content-center "
+                action=""
+              >
+                <div>
+                  <Field
+                    type="text"
+                    placeholder="Username/Email.."
+                    className="form-control"
+                    name="username"
+                  />
+                  <ErrorMessage
+                    name="username"
+                    component="div"
+                    className="error"
+                  />
+                </div>
+                <div>
+                  <Field
+                    type="text"
+                    placeholder="Password.."
+                    className="form-control"
+                    name="password"
+                  />
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className="error"
+                  />
+                </div>
 
-            <input
-              type="text"
-              placeholder="Password.."
-              className="form-control"
-            />
-
-            <button type="submit" className="btn ">
-              Sign In
-            </button>
-          </form>
+                <button
+                  type="submit"
+                  className="btn "
+                
+                >
+                  Sign In
+                </button>
+              </form>
+            )}
+          </Formik>
         </div>
         <div className="col d-flex justify-content-center">
           <img className="img-fluid" src={animePhoto} alt="" />
