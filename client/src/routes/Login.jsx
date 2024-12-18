ff44e8e2fa184dcfd720d3426aec626895bc42f7
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2024-12-18 12:08:15 +0300

    an alert when logged in and failed

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./login.css";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import animePhoto from "../assets/17f4ec064cad10da1739a11d9b293c09-removebg-preview.png";
 import { Field, Form, ErrorMessage, Formik } from "formik";
 import * as Yup from "yup";
@@ -9,6 +9,8 @@ import axios from "axios";
 function Login() {
   const navigate = useNavigate();
   const base_url = "http://127.0.0.1:5000";
+  const [loggedInAlert, setLoggedInAlert] = useState(false);
+  const [failedLoginAlert, setFailedLoginAlert] = useState(false);
 
   const handleLogin = async (values) => {
     console.log(values);
@@ -16,9 +18,18 @@ function Login() {
       const response = await axios.post(`${base_url}/login`, values, {
         headers: { "Content-Type": "application/json" },
       });
+      setLoggedInAlert(true);
+      setTimeout(() => {
+        setLoggedInAlert(false);
+      }, 3000);
       console.log("response", response.data);
     } catch (error) {
-      console.error(error);
+      let { data, status } = error.response;
+      console.log(data);
+      console.log(status);
+      if (status == 401) {
+        setFailedLoginAlert(true);
+      }
     }
   };
 
@@ -43,6 +54,31 @@ function Login() {
         <div className="col-12 col-md-6 d-flex flex-column align-items-center py-5">
           <div className="the-title">
             <h1 className="display-5 m-0">Welcome Back</h1>
+            {loggedInAlert && (
+              <div
+                class="alert alert-success d-flex align-items-center justify-content-around"
+                role="alert"
+                style={{ maxHeight: "64px" }}
+              >
+                <i class="fa-solid fa-circle-check"></i>
+                <div>Login Success</div>
+              </div>
+            )}
+            {failedLoginAlert && (
+              <div
+                class="alert alert-danger d-flex align-items-center justify-content-around"
+                role="alert"
+                style={{ maxHeight: "64px" }}
+              >
+                <i class="fa-solid fa-circle-xmark"></i>{" "}
+                <div className="d-flex flex-column">
+                  <p className="m-0">Invalid Details </p>
+                  <p className="m-0">
+                    Try again or <NavLink to={"/register"}>Register</NavLink>
+                  </p>
+                </div>
+              </div>
+            )}
             <p>
               Don't Have An Account?{" "}
               <span className="the-link" onClick={() => navigate("/register")}>
@@ -59,6 +95,7 @@ function Login() {
               <form
                 className="form-group d-flex flex-column gap-4 rounded auth justify-content-center"
                 onSubmit={formik.handleSubmit}
+                onChange={() => setFailedLoginAlert(false)}
               >
                 <div>
                   <Field
