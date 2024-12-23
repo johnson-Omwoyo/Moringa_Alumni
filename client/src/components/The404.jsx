99e1f109144b1d5e03e274cb6384ca635c320175
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2024-12-23 07:59:03 +0300

    use error instead of the home page

--- client/src/components/The404.jsx
+++ client/src/components/The404.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import "./the404.css";
 import { Navigate } from "react-router-dom";
 function The404() {
-  return <Navigate to="/" />;
+  return <div>Some 404 here</div>;
+  // return <Navigate to="/" />;
 }
 
 export default The404;
